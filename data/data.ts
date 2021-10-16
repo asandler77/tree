0141af1e0e73a2968bcd228e8cfd4028cee01055
Sandler, Alexey(as371x) <as371x@intl.att.com>
2021-10-16 21:11:33 +0300

    Add relatives component.

---
+++ data/data.ts
@@ -0,0 +1,30 @@
+export const DATA = [
+  {
+    id: '0',
+    name: 'Vladimir',
+    sureName: 'Ulianov',
+    father: 'Ilia',
+    mother: 'Maria',
+    imageUri: 'https://www.biography.com/.image/t_share/MTIwNjA4NjMzODgyNTEwODYw/vladimir-lenin-9379007-1-402.jpg',
+  },
+  {
+    id: '1',
+    name: 'Ilia',
+    sureName: 'Ulianov',
+    father: 'Alexander',
+    mother: 'Anna',
+    child: 'Vladimir',
+    wife: 'Maria',
+    imageUri: 'https://avatars.mds.yandex.net/get-zen_doc/195350/pub_5b2cfb51c2fba000aa1dddbe_5b2d0683e5a60f00a800480f/scale_1200',
+  },
+  {
+    id: '2',
+    name: 'Maria',
+    sureName: 'Ulianov',
+    father: 'Evgeny',
+    mother: 'Katya',
+    child: 'Vladimir',
+    husband: 'Ilia',
+    imageUri: 'https://photos.geni.com/p12/6b/84/20/6c/534448388c483d59/mab_original.jpg',
+  },
+];
